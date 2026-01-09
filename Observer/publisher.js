1da9f80c910f098aafac523c8758b923623ee0aa
Pedro Mendes <pedromendes@alvoarlacteos.com.br>
2026-01-09 17:31:26 -0300

    Novo Update Project

---
+++ Observer/publisher.js
@@ -0,0 +1,19 @@
+class Mainlibrary {
+    #subcribers = [];                // # indica que é um campo privado de classe, só pode ser avessado dentro da própria classe e não fora dela.
+
+    addNewBokk(book){
+        this.#subcribers.forEach((sub) => {                 // this = "este objeto aqui"
+
+        })                 
+    }
+}
+
+
+
+
+
+() => {
+    return a + b;
+}
+
+(2, 3)
